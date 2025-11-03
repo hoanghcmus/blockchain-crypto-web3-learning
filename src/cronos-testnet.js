28ec9b654283bcd7ee8660e927e0d557c5e36cdd
Steve Nguyen <hoanghcmus@gmail.com>
2025-11-03 19:52:27 +0700

    feat: BCW send ETH testnet

--- src/cronos-testnet.js
+++ src/cronos-testnet.js
@@ -1,31 +1,62 @@
 import { ethers } from "ethers";
 
-// ====== Cấu hình mạng Cronos Testnet ======
+// ====== Setup CRONOS RPC provider======
 const CRONOS_TESTNET_RPC = "https://evm-t3.cronos.org";
-const provider = new ethers.JsonRpcProvider(CRONOS_TESTNET_RPC);
+const cronosProvider = new ethers.JsonRpcProvider(CRONOS_TESTNET_RPC);
 
-// ====== Ví của bạn ====== CRO Testnet
-const PRIVATE_KEY = ""; // ⚠️ chỉ dùng ví test!
-const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-// ====== Địa chỉ người nhận ====== CRYPTO.COM
-const toAddress = "0x12D10B2314cC42FB42Ba8b594020b6B280C244fa";
+// ====== CRONOS Wallet ====== CRO Testnet
+const CRONOS_PRIVATE_KEY = "PRIVATE_KEY_HERE"; // ⚠️ Only use test wallet!
+const cronosWallet = new ethers.Wallet(CRONOS_PRIVATE_KEY, cronosProvider);
 
-// ====== Hàm gửi giao dịch ======
+// ====== CRONOS Receive address ====== CRO Faucet
+const toCronosAddress = "0x12D10B2314cC42FB42Ba8b594020b6B280C244fa";
+
+// ====== Send CRO on tesnet ======
 async function sendTCRO() {
-    const amount = ethers.parseEther("1.0"); // 1 tCRO
-    console.log(`Đang gửi 1 tCRO đến ${toAddress}...`);
 
-    const tx = await wallet.sendTransaction({
-        to: toAddress,
-        value: amount,
+    console.log(`Sending 1 tCRO to ${toCronosAddress}...`);
+    const tx = await cronosWallet.sendTransaction({
+        to: toCronosAddress,
+        value: ethers.parseEther("1.0"), // 1 tCRO
     });
 
-    console.log("Đã gửi, đang chờ xác nhận...");
+    console.log("Sent, waiting for confirm...");
     const receipt = await tx.wait();
-    console.log("✅ Giao dịch thành công!");
+    console.log("✅ Successfull CRO transaction!");
     console.log(`Tx Hash: ${receipt.hash}`);
-    console.log(`Xem tại: https://explorer.cronos.org/testnet/address/0xb82dc9d470ccd9f40d6d8ef74a3d006c1959a90d`);
+    console.log(`Review at: https://explorer.cronos.org/testnet/address/0xb82dc9d470ccd9f40d6d8ef74a3d006c1959a90d`);
 }
 
-sendTCRO().catch(console.error);
+
+// Setup Sepolia RPC provider
+// const ETH_TESTNET_RPC = "https://rpc.sepolia.org";
+const ETH_TESTNET_RPC = "https://eth-sepolia.public.blastapi.io/"
+const ethPpethrovider = new ethers.JsonRpcProvider(ETH_TESTNET_RPC);
+
+// Load ETH wallet (⚠️ keep private key safe!)
+const ethPrivateKey = "PRIVATE_KEY_HERE";
+const ethWallet = new ethers.Wallet(ethPrivateKey, ethPpethrovider);
+
+// ====== CRONOS Receive address ====== CRO Faucet
+const toEthAddress = "0xa7e4ef0a9e15bdef215e2ed87ae050f974ecd60b";
+
+// Send ETH transaction
+async function sendEth() {
+    console.log(`Sending 1 sepoliaETH to ${toEthAddress}...`);
+    const tx = await ethWallet.sendTransaction({
+        to: toEthAddress, // Sepolia faucet
+        value: ethers.parseEther("0.001"), // 0.001 ETH
+    });
+
+    console.log("Sent, waiting for confirm...");
+    const receipt = await tx.wait();
+    console.log("Transaction hash:", tx.hash);
+    console.log("✅ Transaction confirmed in block:", receipt.blockNumber);
+    console.log(`Review at: https://sepolia.etherscan.io/address/0xB82DC9d470ccD9f40d6d8ef74A3d006C1959A90D`);
+}
+
+
+// sendTCRO().catch(console.error);
+sendEth().catch(console.error);
+
